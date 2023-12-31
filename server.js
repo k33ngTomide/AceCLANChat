9906ec45c8a7c2fc2b7121e3249cbd61d01dd82a
k33ngTomide <Msodiqakintomide@gmail.com>
2023-12-31 01:35:18 +0100

    Added backend

---
+++ server.js
@@ -0,0 +1,66 @@
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import the cors middleware
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors({
+  origin: 'http://localhost:3000/', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,            
+}));
+
+mongoose.set('strictQuery', true);
+
+
+mongoose.connect('mongodb://localhost:27017/chat', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, () => {
+  console.log("Database connected");
+});;
+
+
+const Message = mongoose.model('Message', {
+  text: String,
+});
+
+io.on('connection', async (socket) => {
+  try {
+    console.log('new user connected');
+
+    const messages = await Message.find({});
+    socket.emit('load messages', messages);
+
+
+    socket.on('send message', (data) => {
+      const {text} = data;
+  
+      const message = new Message({text});
+      message.save().then(() => {
+        io.emit('receive message', text);
+      });
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('one user disconnected');
+    });
+
+  } catch (error) {
+    console.error('Error in connection event:', error);
+  }
+});
+
+server.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
+});
